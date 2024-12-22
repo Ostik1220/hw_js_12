39eb7072c1d22ffa050cd3472718842b0bd09360
Ostik <117195748+Ostik1220@users.noreply.github.com>
2024-12-22 17:32:01 +0200

    save

--- place.js
+++ place.js
@@ -40,6 +40,52 @@ console.log(countProps(commands))
 
 
 //3
-function findBestEmployee() {
+
+const employees = [
+    {name: "Andrii", tasks: 34, salary: 15100, mood:"happy"},
+    {name: "Marta", tasks: 26, salary: 14300, mood:"angry"},
+    {name: "Jan", tasks: 35, salary: 14500, mood:"tired"},
+    {name: "Oleg", tasks: 12, salary: 8700,mood: "happy"}
+]
+
+
+function findBestEmployee(arr, tasks) {
+let max = 0; 
+let bestEmployee = ""; 
+
+for (let employee of arr) {
+  if (employee.tasks > max) {
+    max = employee.tasks;
+    bestEmployee = employee.name; 
+  }
+}
+
+return bestEmployee;
 
 }
+
+console.log(findBestEmployee(employees))
+
+//4
+
+function countTotalSalary (arr) {
+    let middleSalary = 0;
+
+    for (let employee of arr) {
+            middleSalary += employee.salary           
+        
+    }
+    middleSalary = middleSalary / arr.length
+    return middleSalary
+}
+
+console.log(countTotalSalary(employees))
+
+
+//5
+
+function getAllPropValues(arr, prop) {
+
+}
+
+console.log(countTotalSalary(employees))
